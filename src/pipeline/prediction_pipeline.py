da66446de331d86973141042ef996a4989252a86
daredevil007-aks <chourasiaakshat2@gmail.com>
2023-12-01 14:53:46 +0530

    prediction_pipeline completed

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,6 +1,7 @@
 from json import load
 import os
 import sys
+from turtle import distance
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import load_object
@@ -36,4 +37,35 @@ class CustomData:
                  seller_type:str,
                  transmission:str,
                  owner:str):
+        self.year = year
+        self.km_driven = km_driven
+        self.mileage = mileage
+        self.engine = engine
+        self.max_power = max_power
+        self.seats = seats
+        self.fuel = fuel
+        self.seller_type = seller_type
+        self.transmission = transmission
+        self.owner = owner
+
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'year':[self.year],
+                'km_driven':[self.km_driven],
+                'mileage':[self.mileage],
+                'engine':[self.engine],
+                'max_power':[self.max_power],
+                'seats':[self.seats],
+                'fuel':[self.fuel],
+                'seller_type':[self.seller_type],
+                'transmission':[self.transmission],
+                'owner':[self.owner]
+            }        
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('DataFrame Gathered')
+            return df
         
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e, sys)
